bfd7fc5895b7ffbd8f1e12197c4bc3d729eb5018
OuyangJing <ouyangj999@gmail.com>
2023-10-13 09:55:01 +0800

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,27 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function get(context) {
 	const posts = await getCollection('blog');
+	const notes = await getCollection('note');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
+			content: sanitizeHtml(parser.render(post.body)),
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
+		items: notes.map((post) => ({
+			content: sanitizeHtml(parser.render(post.body)),
+			...post.data,
+			link: `/blog/${post.slug}/`,
+		}))
 	});
+
 }
