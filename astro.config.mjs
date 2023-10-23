99b7ce52868dac5d3f6e568e56d352a57d65d9a7
OuyangJing <ouyangj999@gmail.com>
2023-10-23 12:30:00 +0800

    toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,21 @@ import remarkMath from 'remark-math'
 import remarkRehype from 'remark-rehype'
 import rehypeKatex from 'rehype-katex'
 import rehypeStringify from 'rehype-stringify'
-//import remarkToc from 'remark-toc';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+
+
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://joviisaus.github.io',
 	markdown:{
-		remarkPlugins: [remarkParse,remarkMath,remarkRehype],
-		rehypePlugins: [rehypeKatex,rehypeStringify]
-	  },
+		remarkPlugins: [remarkParse,remarkMath,remarkRehype,[remarkToc, { heading: "contents" }]],
+		rehypePlugins: [rehypeKatex,rehypeStringify,rehypeHeadingIds,rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append'}]],
+	},
 	integrations: [mdx(), sitemap()],
 
 });
