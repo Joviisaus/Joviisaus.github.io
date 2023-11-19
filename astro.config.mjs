ab6719e9c324e81b139cb79b35468b7856ef9560
OuyangJing <ouyangj999@gmail.com>
2023-11-19 21:23:30 +0800

    code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import remarkParse from 'remark-parse'
-import remarkMath from 'remark-math'
-import remarkRehype from 'remark-rehype'
-import rehypeKatex from 'rehype-katex'
-import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse';
+import remarkMath from 'remark-math';
+import remarkRehype from 'remark-rehype';
+import rehypeKatex from 'rehype-katex';
+import rehypeStringify from 'rehype-stringify';
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import { remarkReadingTime } from './config.plugin/remark-reading-time.mjs';
 import { remarkModifiedTime } from './config.plugin/remark-modified-time.mjs';
-import 'animate.css'
+import 'animate.css';
 
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://viisaus.tech',
-	markdown:{
-		remarkPlugins: [remarkParse,remarkMath,remarkRehype,[remarkToc, { heading: "contents" }],remarkReadingTime,remarkModifiedTime],
-		rehypePlugins: [rehypeKatex,rehypeStringify,rehypeHeadingIds,rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append'}]],
-	},
-	integrations: [mdx(), sitemap()],
-
+  site: 'https://viisaus.tech',
+  markdown: {
+    remarkPlugins: [remarkParse, remarkMath, remarkRehype, [remarkToc, {
+      heading: "contents"
+    }], remarkReadingTime, remarkModifiedTime],
+    rehypePlugins: [rehypeKatex, rehypeStringify, rehypeHeadingIds, rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: 'append'
+    }]]
+  },
+  integrations: [ expressiveCode(), mdx(), sitemap()]
 });
-
-
