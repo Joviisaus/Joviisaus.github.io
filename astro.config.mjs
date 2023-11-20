8e620fd38a1748011692ac2abdab235aeb14c495
OuyangJing <ouyangj999@gmail.com>
2023-11-20 14:49:40 +0800

    code-theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,18 +14,36 @@ import { remarkReadingTime } from './config.plugin/remark-reading-time.mjs';
 import { remarkModifiedTime } from './config.plugin/remark-modified-time.mjs';
 import 'animate.css';
 
-import expressiveCode from "astro-expressive-code";
+import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://viisaus.tech',
   markdown: {
+    shikiConfig: {
+      // 选择 Shiki 内置的主题（或添加你自己的主题）
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // 另外，也提供了多种主题
+      // https://github.com/antfu/shikiji#lightdark-dual-themes
+      experimentalThemes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // 添加自定义语言
+      // 注意：Shiki 内置了无数语言，包括 .astro！
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ['java','c','c++','py'],
+      // 启用自动换行，以防止水平滚动
+      wrap: true,
+    },
     remarkPlugins: [remarkParse, remarkMath, remarkRehype, [remarkToc, {
       heading: "contents"
     }], remarkReadingTime, remarkModifiedTime],
     rehypePlugins: [rehypeKatex, rehypeStringify, rehypeHeadingIds, rehypeSlug, [rehypeAutolinkHeadings, {
       behavior: 'append'
-    }]]
+    }]],
+    extendDefaultPlugins: true
   },
-  integrations: [ expressiveCode(), mdx(), sitemap()]
+  integrations: [ astroExpressiveCode(), mdx(), sitemap()]
 });
